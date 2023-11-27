140e10853506147a476ddd8fb634b983cc4a5748
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-27 21:53:44 +0700

    tambah route ubah gambar profil

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { updateProfilPicture } = require("../controllers/user_controller");
+const { Auth } = require("../middlewares/jwt");
 
 const multer = require("multer")();
-router.post("/update-image", multer.single("image"), updateProfilPicture);
+router.post("/update-image", multer.single("image"), Auth, updateProfilPicture);
 
 module.exports = router;
