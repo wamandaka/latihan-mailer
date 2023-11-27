04c6c781e40b084150c1264f98564c344c27e2ac
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-27 20:40:55 +0700

    update code

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const morgan = require("morgan");
 const auth = require("./auth_route");
+const userRoute = require("./user_route");
 
-router.use(morgan("morgan"));
+router.use(morgan("dev"));
 router.use("/auth", auth);
+router.use("/user", userRoute)
 module.exports = router;
